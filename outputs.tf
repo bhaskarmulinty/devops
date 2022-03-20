04d6d3dc128be3a8282fd58ba4511abf15d98561
bhaskarmulinty <73287046+bhaskarmulinty@users.noreply.github.com>
2022-03-20 11:53:00 +0530

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "resource_group" {
-  value = azurerm_resource_group.main
-}
-output "storage_account" {
-  value = azurerm_resource_group.main.name
-}
-output "location" {
-  value = azurerm_storage_account.main.location
-}
